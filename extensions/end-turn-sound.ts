88e73072e574b450a681e15304f5aaae1c468c1a
hyongew <hongyew98@gmail.com>
2026-09-23 23:06:10 +0800

    dont play notification sound if cancelled before response end

--- extensions/end-turn-sound.ts
+++ extensions/end-turn-sound.ts
@@ -7,7 +7,24 @@ function playBellSound(ctx: ExtensionContext): void {
 }
 
 export default function (pi: ExtensionAPI): void {
+	let turnWasAborted = false;
+
+	pi.on("agent_start", () => {
+		turnWasAborted = false;
+	});
+
+	pi.on("agent_end", (event, ctx) => {
+		turnWasAborted ||=
+			ctx.signal?.aborted === true ||
+			event.messages.some((message) => message.role === "assistant" && message.stopReason === "aborted");
+	});
+
+	pi.on("agent_before_settle", (event) => {
+		turnWasAborted ||= event.outcome === "aborted";
+	});
+
 	pi.on("agent_settled", (_event, ctx) => {
-		playBellSound(ctx);
+		if (!turnWasAborted) playBellSound(ctx);
+		turnWasAborted = false;
 	});
 }
